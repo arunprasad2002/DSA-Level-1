bbf89da5d4279ba72bc15705c05659d43c0c4b77
arun mahato <ptech784@gmail.com>
2023-12-25 13:26:49 +0530

    alternative solution for NGER

--- stack/next_largest_element_to_right.py
+++ stack/next_largest_element_to_right.py
@@ -1,17 +1,28 @@
-def next_larget_element(arr):
-    ans = []
+
+def next_greater_element_right(arr):
+    ans = [0] * len(arr)
+    stack = []
+    for i in range(len(arr) - 1, -1, -1):
+        while len(stack) > 0 and arr[i] > stack[-1]:
+            stack.pop()
+        if len(stack) == 0:
+            ans[i] = -1
+        else:
+            ans[i] = stack[-1]
+        stack.append(arr[i])
+    return ans
+
+def next_greater_element_rightV2(arr):
+    ans = [-1] * len(arr)
+    stack = []
     for i in range(len(arr)):
-        current_element = arr[i]
-        for j in range(i + 1, len(arr)):
-            next_element = arr[j]
-            if current_element < next_element:
-                ans.append(next_element)
-                break
-    ans.append(-1)
+        while stack and arr[i] > arr[stack[-1]]:
+            index = stack.pop()
+            ans[index] = arr[i]
+        stack.append(i)
     return ans
 
 
-
-arr = [1,3,0,0,1,2,4]
-ans = next_larget_element(arr)
+arr = [1,0,2,3]
+ans = next_greater_element_rightV2(arr)
 print(ans)
