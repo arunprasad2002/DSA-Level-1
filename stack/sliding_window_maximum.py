2a547d4366d929b4777affeea23fdad639efc644
arun mahato <ptech784@gmail.com>
2024-01-03 14:17:44 +0530

    understanded - sliding window maximum for j range in brute force

--- stack/sliding_window_maximum.py
+++ stack/sliding_window_maximum.py
@@ -1,11 +1,13 @@
 def sliding_window_maximum(arr):
     n = len(arr)
-    k = 4  # window size
+    k = 4
 
     for i in range(n - k + 1):
-        window = arr[i:i+k]
-        max_val = max(window)
+        max_val = arr[i]
+        for j in range(i, i + k):
+            if arr[j] > max_val:
+                max_val = arr[j]
         print(max_val, end=" ")
 
 arr = [2, 9, 3, 8, 1, 7, 12, 6, 14, 4, 32, 0, 7, 19, 8, 12, 6]
-sliding_window_maximum(arr) 
+sliding_window_maximum(arr)
