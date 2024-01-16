231dc8df6abdd463558225527f354239d6e05a9f
arun mahato <ptech784@gmail.com>
2024-01-16 14:57:14 +0530

    solved - find celebrity

--- stack/find_celebrity.py
+++ stack/find_celebrity.py
@@ -22,11 +22,42 @@ def find_celebrity(peoples):
     
     return -1
 
+def find_celebrityV2(peoples):
+    incoming = [0] * len(peoples)
+    outgoing = [0] * len(peoples)
+    for i in range(len(peoples)):
+        for j in range(len(peoples)):
+            if peoples[i][j] == 1:
+                incoming[j] += 1
+                outgoing[i] += 1
+    for k in range(len(incoming)):
+        if incoming[k] == len(peoples) - 1 and outgoing[k] == 0:
+            return k
+    
+    return -1
+
+def find_celebrityV3(peoples):
+    celebrity = 0
+    
+    # Find a potential celebrity
+    for i in range(1, len(peoples)):
+        if peoples[celebrity][i] == 1:
+            celebrity = i
+    
+    # Check if the potential celebrity meets the criteria
+    for j in range(len(peoples)):
+        if j != celebrity and (peoples[celebrity][j] == 1 or peoples[j][celebrity] == 0):
+            return -1
+    
+    return celebrity
+
+
+
 peoples = [
     [0, 1, 0],
     [0, 0, 0],
     [0, 1, 0]
 ]
 
-ans = find_celebrity(peoples)
+ans = find_celebrityV2(peoples)
 print(ans)
